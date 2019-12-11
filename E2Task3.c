3380c77ba4a959814b3b1e0b05f5649db321cbc2
huijinyuan <huijinyuan@hotmail.com>
2019-12-11 23:18:59 +0200

    Update E2Task3.c
    Add \n for each result.

--- E2Task3.c
+++ E2Task3.c
@@ -10,32 +10,32 @@ the grade corresponding to the given score
 int main(int argc, char *argv[]) {
 	int score;
 	do{
-		printf("\nInput the score (0-24):");
+		printf("\nInput the score (0-24):\n");
 		scanf("%d", &score);
 		if(score<0||score>24){
-			printf("\nYou have input invalid number.");			
+			printf("\nYou have input invalid number.\n");			
 		}
 		
 	}while(score<0||score>24);
 	
 	switch(score){
 		case 0 ... 11: 
-		printf("\nYour are fail.");
+		printf("\nYou are fail.\n");
 		break;
 		case 12 ... 13: 
-		printf("\nYour grade is 1.");
+		printf("\nYour grade is 1.\n");
 		break;
 		case 14 ... 16: 
-		printf("\nYour grade is 2.");
+		printf("\nYour grade is 2.\n");
 		break;
 		case 17 ... 19: 
-		printf("\nYour grade is 3.");
+		printf("\nYour grade is 3.\n");
 		break;
 		case 20 ... 22: 
-		printf("\nYour grade is 4.");
+		printf("\nYour grade is 4.\n");
 		break;
 		case 23 ... 24: 
-		printf("\nYour grade is 5");
+		printf("\nYour grade is 5. \n");
 		break;
 		
 	}
